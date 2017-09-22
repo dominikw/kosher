eb123ca9300903025aa9e18152eb17e8c5139539
Dominik Wojciechowski <dominik.wojciechowski@netguru.co>
2017-09-22 13:52:44 +0200

    Require location
    RHIZ-488

--- lib/kosher.rb
+++ lib/kosher.rb
@@ -3,6 +3,6 @@ require 'money'
 require 'virtus'
 
 # Internal dependencies.
-%w(price dispatch item seller offer serializer).each do |model|
+%w(price dispatch item location seller offer serializer).each do |model|
   require "kosher/#{model}"
 end
